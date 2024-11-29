b50d8943565cc10b625dd880ab09537bc26bd457
teddyaldridge <teodora_aldridge@yahoo.com>
2024-11-29 14:34:49 +0000

    create auth routes and add auth routes to server

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ dotenv.config();
 const cors = require("cors");
 const port = process.env.PORT;
 const connectDB = require("./db/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/", authRoutes);
 
 connectDB();
 
