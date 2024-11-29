60e86fab6fe251d22def20b3605bad68d7479887
teddyaldridge <teodora_aldridge@yahoo.com>
2024-11-29 13:54:36 +0000

    connect to database

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
 const port = process.env.PORT;
+const connectDB = require("./db/db");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+connectDB();
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
